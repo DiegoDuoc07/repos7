5bc8e7007e26f4d0c9d07c9f6841de13f3031012
Diego <dieg.otarola@duocuc.cl>
2025-04-29 10:41:32 -0400

    Primer cambio

---
+++ repos7/src/test/java/com/repos7/repos7/Repos7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repos7.repos7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repos7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
